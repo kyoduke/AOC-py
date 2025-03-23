fb1aac38710aecad181a6efab0cc9239af6eb64b
Caio Duque <caioduque.dev@gmail.com>
2025-03-23 00:15:04 -0300

    day 01 part 01 solution

---
+++ 2024/solutions/day01/p1.py
@@ -0,0 +1,16 @@
+with open('input.txt', 'r') as file:
+    first_col, second_col = [], []
+    for line in file.readlines():
+        _first_col, _second_col = line.split('   ')
+        first_col.append(int(_first_col))
+        second_col.append(int(_second_col))
+
+    first_col.sort()
+    second_col.sort()
+
+    distances = []
+    for i, num in enumerate(first_col):
+        distance = abs(num - second_col[i])
+        distances.append(distance)
+
+    print(sum(distances))
